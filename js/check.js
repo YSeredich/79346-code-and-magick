82e0900fdd63b1c9d7dcb367f08eb439c937fe21
ÐYulia Seredich <yseredich@gmail.com>
2015-11-13 15:58:03 +0300

    add getMessage

---
+++ js/check.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Julia on 12.11.2015.
+ */
+function getMessage(a,b) {
+  var message;
+
+  if (typeof a == boolean) {
+    if (a == true) {
+      message = "Я попал в [b]";
+    } else {
+      message = "Я никуда не попал";
+    }
+  }
+
+  else if (typeof a == number) {
+    message = "Я прыгнул на [a] * 100 сантиметров";
+  }
+
+  else if (typeof a == object && typeof b !== object) {
+    var sum;
+    for (var i = 0; i < a.length; i++) {
+      sum += a[i];
+    }
+    message = "Я прошёл [sum] шагов";
+  }
+
+  else if (typeof a == object && typeof b == object) {
+    var length;
+    for (var j = 0; j < a.length; j++) {
+      length += a[j]*b[j];
+    }
+    message = "Я прошёл [length] метров"
+  }
+  return message;
+}
