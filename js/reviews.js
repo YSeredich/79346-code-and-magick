ea15510f79e3096924fb47a1823be717511ab1ab
ÐYulia Seredich <yseredich@gmail.com>
2015-11-27 04:41:32 +0300

    did small preparation

---
+++ js/reviews.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Julia on 27.11.2015.
+ */
+'use strict';
+(function() {
+  var reviewsFilter = document.querySelector('.reviews-filter');
+  reviewsFilter.classList.add('invisible');
+})();
