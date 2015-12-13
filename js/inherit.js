8122ce17e8503f8fa5f5e1da7604e6aeb1048da5
ÐYulia Seredich <yseredich@gmail.com>
2015-12-14 00:30:45 +0300

    add function inherit

---
+++ js/inherit.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Julia on 14.12.2015.
+ */
+'use strict';
+/**
+ * Функция записывает в прототип дочернего конструктора child
+ * методы и свойства родительского конструктора parent
+ * @param {Function} child
+ * @param {Function} parent
+ */
+(function inherit(child, parent) {
+  /**
+   * @constructor
+   */
+  var EmptyConstructor = function() {};
+  EmptyConstructor.prototype = parent.prototype;
+  /**
+   *
+   * @type {EmptyConstructor}
+   */
+  child.prototype = new EmptyConstructor();
+})();
